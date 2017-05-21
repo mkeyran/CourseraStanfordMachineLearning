45c133bce582ed8b9185bef0b193f2ef7decf401
Keyran <m.keyran@gmail.com>
2017-05-21 14:07:10 +0300

    Ninth week, sixth assignment

--- XVI. Recommender Systems (Week 9)/machine-learning-ex8/ex8/cofiCostFunc.m
+++ XVI. Recommender Systems (Week 9)/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -42,8 +42,8 @@ Theta_grad = zeros(size(Theta));
 
 
 J = sum(((X*Theta' - Y).^2.*R)(:))/2 + lambda/2 * (sum ((Theta.^2)(:)) + sum((X.^2)(:))) ;
-X_grad = ((X*Theta' - Y).*R)*Theta;
-Theta_grad = ((X*Theta' - Y).*R)'*X;
+X_grad = ((X*Theta' - Y).*R)*Theta + lambda * X;
+Theta_grad = ((X*Theta' - Y).*R)'*X + lambda * Theta;
 
 
 
